742cdedd3d4bf98da2a6134d190fb3c41ef8e66e
Asaph TK Binene <asaph@binene.com>
2021-07-20 03:30:39 +0200

    change div diplay to table

--- src/App.js
+++ src/App.js
@@ -11,24 +11,44 @@ export default class App extends Component {
   }
 
   isEven(val) {
-    return val % 2 === 0 ? "Even" : "Odd";
+    return val % 2 === 0 ? "Yes it is an Even number" : "No it is an Odd number";
   }
 
   getClassName(val) {
       return val % 2 === 0
-        ? "bg-primary text-white text-center p-2 m-1"
-        : "bg-danger text-white text-center p-2 m-1"
+        ? "row bg-light text-center p-2"
+        : "row bg-danger text-center p-2"
   }
 
   handleClick = () => this.setState({count: this.state.count + 1});
 
   render = () => 
-    <h4 className={this.getClassName(this.state.count)}>
-      <button className="btn btn-info m-2" onClick={ this.handleClick }>
-          Click Me
-      </button>
-      Number of things is { this.isEven(this.state.count) } number
 
-    </h4>
+    <table className="table table-striped table-bordered table-sm">
+      <thead className="bg-info text-white">
+        <tr>
+          <th>Value</th>
+          <th>Even?</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{ this.state.count }</td>
+          <td>{ this.isEven(this.state.count) }</td>
 
+        </tr>
+
+      </tbody>
+      <tfoot className="text-center">
+        <tr>
+          <td colSpan="2">
+            <button className="btn btn-info m-2"
+                  onClick={ this.handleClick }>
+                    Click Me
+            </button>
+
+          </td>
+        </tr>
+      </tfoot>
+    </table>
 }
